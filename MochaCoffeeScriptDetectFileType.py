a15f5b1132f9e3f3aa5cec8118a3c55e75ed2ce1
Peter DeMartini and Roy van de Water <sqrt+peter+roy@octoblu.com>
2015-02-17 08:43:49 -0700

    Add filetype detection

---
+++ MochaCoffeeScriptDetectFileType.py
@@ -0,0 +1,20 @@
+import sublime, sublime_plugin
+import os
+
+class MochaCoffeeScriptDetectFileTypeCommand(sublime_plugin.EventListener):
+  """ Detects current file type if the file's extension isn't conclusive """
+
+  def on_load(self, view):
+    filename = view.file_name()
+    if not filename: # buffer has never been saved
+      return
+
+    name = os.path.basename(filename.lower())
+    if name.endswith("spec.js.coffee") or name.endswith("spec.coffee"):
+      set_syntax(view, "Mocha CoffeeScript", "Mocha CoffeeScript/Syntaxes")
+
+def set_syntax(view, syntax, path=None):
+  if path is None:
+    path = syntax
+  view.settings().set('syntax', 'Packages/'+ path + '/' + syntax + '.tmLanguage')
+  print("Switched syntax to: " + syntax)
